18d48185da9984406846da16fe2c86f2f1715537
12hellyeah12 <113688239+12hellyeah12@users.noreply.github.com>
2025-08-19 15:48:18 +0200

    Connect UI with the playRound function
    This allows the user to play the game with the UI

--- script.js
+++ script.js
@@ -13,15 +13,18 @@ function gameboard() {
     const columns = 3;
     const board = [];
 
-    for(let i = 0; i < rows; i++){
-        board[i] = [];
-        for(let j = 0; j < columns; j++){
-                board[i].push('-');
+    const initResBoard = () => { 
+        for(let i = 0; i < rows; i++){
+            board[i] = [];
+            for(let j = 0; j < columns; j++){
+                    board[i].push('');
+            }
         }
-    }
+    };
+    const getBoard = () => board;
     const printBoard = () => console.log(board);
     const addToken = (row, col, token) => {
-        if(board[row][col] != '-' || row > 2 || col > 2){
+        if(board[row][col] != '' || row > 2 || col > 2){
             return false;
         } else{
             board[row][col] = token;
@@ -45,12 +48,12 @@ function gameboard() {
             && boardFlat[condition[0]] === boardFlat[condition[2]];
         })
 
-        const isTie = boardFlat.every(token => token != '-');
+        const isTie = boardFlat.every(token => token != '');
 
         return {isWin, isTie};
     };
 
-   return {printBoard, addToken, determineWinner};
+   return {initResBoard, getBoard, printBoard, addToken, determineWinner};
 }
 
 function determinePlayer() {
@@ -72,16 +75,19 @@ function gameController (){
     const players = [
         {
             name: 'playerOne',
-            token: 'X'
+            token: 'X',
+            score: 0
         },
 
         {
             name: 'playerTwo',
-            token: 'O'
+            token: 'O',
+            score: 0
         }
     ];
 
     // Initialize game with a new board and a random player to start.
+    board.initResBoard();
     board.printBoard();
     console.log(`${players[activePlayer].name}'s turn`);
 
@@ -93,22 +99,26 @@ function gameController (){
         board.printBoard();
 
         if(board.determineWinner(activeToken).isWin){
+            players[activePlayer].score += 1;
             console.log('______________________________________');
             console.log(`${players[activePlayer].name} wins!`);
             console.log('______________________________________');
             setTimeout(() => {
                 console.clear();
+                board.initResBoard();
                 console.log('To start Tic Tac Toe type "nameYourGame = startGame()".')
             }, 3000);
-        } else if(board.determineWinner().isTie){
+        } else if(board.determineWinner(activeToken).isTie){
             console.log('______________________________________');
             console.log(`It is a TIE!`);
             console.log('______________________________________');
             setTimeout(() => {
                 console.clear();
+                board.initResBoard();
                 console.log('To start Tic Tac Toe type "nameYourGame = startGame()".')
             }, 3000);
-        }else if(validMove && !board.determineWinner(activeToken).isWin && !board.determineWinner().isTie){
+        }else if(validMove && !board.determineWinner(activeToken).isWin && !board.determineWinner(activeToken).isTie){
+            prevToken = players[player.getPlayer()].token
             activePlayer = player.changePlayer();
             activeToken = players[activePlayer].token;
             console.log(`${players[activePlayer].name}'s turn`);
@@ -116,7 +126,33 @@ function gameController (){
             console.log('Invalid move. Place your token elsewhere!');
             console.log(`${players[activePlayer].name}'s turn`);
         }
+        return board
     };
 
-    return {playRound};
+    return {playRound, players};
 }
+
+
+(function (){
+    const game = gameController()
+    const gameCell = document.querySelectorAll('.game-cell');
+    const scores = document.querySelectorAll('.points');
+
+    const renderGame = (gameBoard) => {
+        for(i = 0; i < gameCell.length; i++){
+            gameCell[i].textContent = gameBoard.getBoard().flat()[i];
+        };
+        scores[0].textContent = game.players[0].score
+        scores[1].textContent = game.players[1].score
+    };
+
+    // Event Listeners
+    gameCell.forEach(cell => cell.addEventListener("click", () => {
+        [row, col] = Array.from(cell.dataset.coordinates);
+        const gameBoard = game.playRound(row, col);
+        renderGame(gameBoard);
+    }));
+})();
+
+
+
